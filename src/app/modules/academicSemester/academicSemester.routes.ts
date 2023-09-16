15acd496923e64728e2bab7dbccb945a7945af09
FaizulOsman <faizulosmanrabby@gmail.com>
2023-09-16 20:49:41 +0600

    feat: academic semester validations added

--- src/app/modules/academicSemester/academicSemester.routes.ts
+++ src/app/modules/academicSemester/academicSemester.routes.ts
@@ -3,18 +3,24 @@ import { AcademicSemesterController } from './academicSemester.controller';
 import validateRequest from '../../middlewares/validateRequest';
 import auth from '../../middlewares/auth';
 import { ENUM_USER_ROLE } from '../../../enums/user';
+import { AcademicSemesterValidation } from './academicSemester.validations';
 
 const router = express.Router();
 
 router.get('/', AcademicSemesterController.getAllFromDB);
 router.get('/:id', AcademicSemesterController.getByIdFromDB);
 
-router.post('/', AcademicSemesterController.insertIntoDB);
+router.post(
+  '/',
+  validateRequest(AcademicSemesterValidation.create),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  AcademicSemesterController.insertIntoDB
+);
 
 router.patch(
   '/:id',
-  // validateRequest(AcademicSemesterValidation.update),
-  //   auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  validateRequest(AcademicSemesterValidation.update),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
   AcademicSemesterController.updateOneIntoDB
 );
 
