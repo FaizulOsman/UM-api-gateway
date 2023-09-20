27244e8d43573e04c66246a36c6a5f3915ad790f
FaizulOsman <faizulosmanrabby@gmail.com>
2023-09-20 12:46:30 +0600

    update: login functionality update

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -1,11 +1,26 @@
 import { NextFunction, Request, Response } from 'express';
 import { AuthenticationService } from './auth.service';
 import sendResponse from '../../../shared/response';
+import config from '../../../config';
+import httpStatus from 'http-status';
 
 const loginUser = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const result = await AuthenticationService.loginUser(req);
-    sendResponse(res, result);
+    const { refreshToken, ...others } = result.data;
+
+    const cookieOptions = {
+      secure: config.env === 'production',
+      httpOnly: true
+    };
+
+    res.cookie('refreshToken', result.data.refreshToken, cookieOptions);
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: 'User Logged in successfully',
+      data: others
+    });
   } catch (error) {
     next(error);
   }
