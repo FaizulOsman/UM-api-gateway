75a9fe770678396e5ab9b44a4d9c247dbc1ba732
FaizulOsman <faizulosmanrabby@gmail.com>
2023-09-20 13:18:31 +0600

    feat: change password functionality increment

--- src/app/modules/auth/auth.route.ts
+++ src/app/modules/auth/auth.route.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import { AuthenticationController } from './auth.controller';
 import validateRequest from '../../middlewares/validateRequest';
 import { AuthValidation } from './auth.validation';
+import auth from '../../middlewares/auth';
+import { ENUM_USER_ROLE } from '../../../enums/user';
 
 const router = express.Router();
 
@@ -14,6 +16,18 @@ router.post(
   '/refresh-token',
   validateRequest(AuthValidation.refreshTokenZodSchema),
   AuthenticationController.refreshToken
+);
+
+router.post(
+  '/change-password',
+  auth(
+    ENUM_USER_ROLE.SUPER_ADMIN,
+    ENUM_USER_ROLE.ADMIN,
+    ENUM_USER_ROLE.STUDENT,
+    ENUM_USER_ROLE.FACULTY
+  ),
+  validateRequest(AuthValidation.changePasswordZodSchema),
+  AuthenticationController.changePassword
 );
 
 export const authRoutes = router;
